640895e5bc5c86bb1c18a5e98c48ba7fea71dc1f
Jolanta Lachman <jolalachman@gmail.com>
2026-04-07 21:51:46 +0200

    update code

--- src/app/tabs/components/tab1/components/generated-sentence/generated-sentence.component.ts
+++ src/app/tabs/components/tab1/components/generated-sentence/generated-sentence.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
+  EventEmitter,
   Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
 } from '@angular/core';
-import { TranslateModule} from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
 import { Tile } from 'src/app/models/tile.model';
+import { TextToSpeech } from '@capacitor-community/text-to-speech';
 import { IonIcon, IonSpinner } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { volumeHighSharp } from 'ionicons/icons';
@@ -17,12 +23,36 @@ import { volumeHighSharp } from 'ionicons/icons';
   standalone: true,
   imports: [TranslateModule, CommonModule, IonIcon, IonSpinner],
 })
-export class GeneratedSentenceComponent {
+export class GeneratedSentenceComponent implements OnInit, OnChanges {
   @Input() selectedTiles$: Observable<Tile[]> = of([]);
-  @Input() playMode: boolean = false;
-
-  constructor() {
+  @Input() generatedSentence: string | null = null;
+  @Output() speakEnded = new EventEmitter<boolean>();
+  constructor(private translate: TranslateService) {
     addIcons({ volumeHighSharp });
   }
-  
+  playMode: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['generatedSentence'].currentValue) {
+      this.playMode = true;
+      this.speak(this.generatedSentence);
+    }
+  }
+
+  ngOnInit() {}
+
+  speak = (sentence: string | null) => {
+    TextToSpeech.speak({
+      text: sentence ?? '',
+      lang: "en-US",
+      rate: 1.0,
+      pitch: 1.0,
+      volume: 1.0,
+      category: 'ambient',
+      queueStrategy: 1,
+    }).then(() => {
+      this.playMode = false;
+      this.speakEnded.next(true);
+    });
+  };
 }
